08bc37cbdb7a02445f2eede5df8dabd943f85b0b
Natalya Demidenko <natalucia777@ukr.net>
2023-05-28 21:02:07 +0300

    profile-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import "./App.css";
+
+import user from '../path/to/user.json';
+import data from '../path/to/data.json';
+import friends from '../path/to/friends.json';
+import transactions from '../path/to/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -10,6 +17,17 @@ export const App = () => {
         color: '#010101'
       }}
     >
+      <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      />
+      <Statistics />
+
+      <FriendList />
+      <TransactionHistory />
       React homework template
     </div>
   );
